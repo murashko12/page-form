f066957a741deaafb3784cb2f2e1e9bf8e0bba16
murashko12 <petr@ocumare.ru>
2025-06-10 14:04:56 +0300

    StatsCards're ready

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,8 +1,11 @@
 import { GetFormStats } from "@/actions/form"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Skeleton } from "@/components/ui/skeleton";
-import { ReactNode, Suspense } from "react";
-import { LuView } from "react-icons/lu";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Skeleton } from "@/components/ui/skeleton"
+import { ReactNode, Suspense } from "react"
+import { LuView } from "react-icons/lu"
+import { FaWpforms } from "react-icons/fa"
+import { HiCursorClick } from "react-icons/hi"
+import { TbArrowBounce } from "react-icons/tb"
 
 export default function Home() {
   return (
@@ -35,24 +38,34 @@ function StatsCards(props: StatsCardProps) {
         helperText="All time form visits"
         value={data?.visits.toLocaleString() || ""}
         loading={loading}
-        className=" shadow-md shadow-blue-600"
+        className="shadow-md shadow-blue-600"
       />
       
       <StatsCard 
         title="Total submissions" 
-        icon={<LuView className="text-yellow-600" />}
-        helperText="All time form visits"
+        icon={<FaWpforms className="text-yellow-600" />}
+        helperText="All time form submissions"
         value={data?.submissions.toLocaleString() || ""}
         loading={loading}
-        className=" shadow-md shadow-yellow-600"
+        className="shadow-md shadow-yellow-600"
       />
+
       <StatsCard 
         title="Submission rate" 
-        icon={<LuView className="text-green-600" />}
-        helperText="All time form visits"
-        value={data?.visits.toLocaleString() || ""}
+        icon={<HiCursorClick className="text-green-600" />}
+        helperText="Visits that result in form submission"
+        value={data?.submissionRate.toLocaleString() || ""}
         loading={loading}
-        className=" shadow-md shadow-green-600"
+        className="shadow-md shadow-green-600"
+      />
+
+      <StatsCard 
+        title="Bounce rate" 
+        icon={<TbArrowBounce className="text-red-600" />}
+        helperText="Visits that leaves without interacting"
+        value={data?.submissionRate.toLocaleString() || ""}
+        loading={loading}
+        className="shadow-md shadow-red-600"
       />
     </div>
   )

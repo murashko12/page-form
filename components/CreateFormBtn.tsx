1680f0cc67aa6d6afe37e1b6a6acd5c82407fa66
murashko12 <petr@ocumare.ru>
2025-06-11 23:22:05 +0300

    style create form button

--- components/CreateFormBtn.tsx
+++ components/CreateFormBtn.tsx
@@ -70,7 +70,13 @@ function CreateFormBtn() {
     return (
         <Dialog>
             <DialogTrigger asChild>
-                <Button>Create new form</Button>
+                <Button 
+                    variant={"outline"}
+                    className="group border border-primary/20 h-[190px] items-center justify-center flex flex-col hover:border-primary hover:cursor-pointer border-dashed gap-4 bg-background"
+                >
+                    <BsFileEarmarkPlus className="text-muted-foreground group-hover:text-primary"/>
+                    <p className="font-bold text-xl text-muted-foreground group-hover:text-primary">Create new form</p>
+                </Button>
             </DialogTrigger>
             <DialogContent>
                 <DialogHeader>

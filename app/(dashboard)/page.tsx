967789274c92513c1ee5df35ae7f9154bb8207d6
murashko12 <petr@ocumare.ru>
2025-06-15 23:09:08 +0300

    form card is ready

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,5 @@
 import { GetForms, GetFormStats } from "@/actions/form"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Skeleton } from "@/components/ui/skeleton"
 import { ReactNode, Suspense } from "react"
 import { LuView } from "react-icons/lu"
@@ -10,6 +10,11 @@ import { Separator } from "@radix-ui/react-context-menu"
 import CreateFormBtn from "@/components/CreateFormBtn"
 import { Form } from "@prisma/client"
 import { Badge } from "@/components/ui/badge"
+import { formatDistance } from "date-fns"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { BiRightArrowAlt } from "react-icons/bi"
+import { FaEdit } from "react-icons/fa"
 
 export default function Home() {
   return (
@@ -154,7 +159,40 @@ function FormCard({ form }: { form: Form }) {
           {form.published && <Badge>Published</Badge>}
           {!form.published && <Badge variant={"destructive"}>Draft</Badge>}
         </CardTitle>
+        <CardDescription className="flex items-center justify-between text-muted-foreground text-sm">
+          {formatDistance(form.createdAt, new Date(), {
+            addSuffix: true
+          })}
+          {!form.published && (
+            <span className="flex items-center gap-2">
+              <LuView className="text-muted-foreground" />
+              <span>{form.visited.toLocaleString()}</span>
+              <FaWpforms className="text-muted-foreground" />
+              <span>{form.submissions.toLocaleString()}</span>
+            </span>
+          )}
+        </CardDescription>
       </CardHeader>
+      <CardContent className="h-[20px] truncate text-sm text-muted-foreground">
+        {form.description || "No description"}
+      </CardContent>
+      <CardFooter>
+        {
+          form.published ? (
+            <Button asChild className="w-full mt-2 text-md gap-4">
+              <Link href={`/forms/${form.id}`}>
+                View submissions <BiRightArrowAlt />
+              </Link>
+            </Button>
+          ) : (
+            <Button asChild className="w-full mt-2 text-md gap-4">
+              <Link href={`/builder/${form.id}`}>
+                Edit form <FaEdit />
+              </Link>
+            </Button>
+          )
+        }
+      </CardFooter>
     </Card>
   )
 }
--- components/CreateFormBtn.tsx
+++ components/CreateFormBtn.tsx
@@ -74,7 +74,7 @@ function CreateFormBtn() {
                     variant={"outline"}
                     className="group border border-primary/20 h-[190px] items-center justify-center flex flex-col hover:border-primary hover:cursor-pointer border-dashed gap-4 bg-background"
                 >
-                    <BsFileEarmarkPlus className="text-muted-foreground group-hover:text-primary"/>
+                    <BsFileEarmarkPlus className="!h-[30px] !w-[30px] text-muted-foreground group-hover:text-primary"/>
                     <p className="font-bold text-xl text-muted-foreground group-hover:text-primary">Create new form</p>
                 </Button>
             </DialogTrigger>
